3d2127a8901e496df2f8c78c9a7f1afb679ba85b
Gowtham <gowthamnba09@gmail.com>
2022-01-09 10:37:15 -0800

    Time: 297 ms (13.43%), Space: 14.9 MB (34.11%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -3,36 +3,44 @@ from collections import deque
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         N = len(graph)
-        visited = [False] * N
+        self.visited = [False] * N
+        self.queue = deque()
+        self.answer = True
+        
         for i in range(N):
-            if not visited[i]:
-                set1 = set()
-                set2 = set()
-                queue = deque()
-                queue.append(i)
-                set1.add(i)
+            if not self.visited[i] and self.answer:
+                self.bfs(i, graph)
+                
+        return self.answer
+    
+    
+    def bfs(self, start, graph):
+        set1 = set()
+        set2 = set()
 
+        self.queue.append(start)
+        set1.add(start)
 
-                while queue:
-                    u = queue.popleft()
-                    visited[u] = True
+        while self.queue:
+            u = self.queue.popleft()
+            self.visited[u] = True
 
-                    if u in set1:
-                        add = set2
-                        other = set1
+            if u in set1:
+                add = set2
+                other = set1
+            else:
+                add = set1
+                other = set2
+
+            for v in graph[u]:
+                if not self.visited[v]:
+                    if v not in other:
+                        add.add(v)
+                        self.queue.append(v)
                     else:
-                        add = set1
-                        other = set2
+                        self.answer = False
+                        return
 
-                    for v in graph[u]:
-                        if not visited[v]:
-                            if v not in other:
-                                add.add(v)
-                                queue.append(v)
-                            else:
-                                return False
-
-        return True
 
 
 
