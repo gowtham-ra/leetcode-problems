2f28cd5f60db1249af732ac400cb8d7668019f5e
Gowtham <gowthamnba09@gmail.com>
2022-01-12 18:57:25 -0800

    Time: 264 ms (67.52%), Space: 18 MB (27.98%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,21 +1,52 @@
+class Bucket:
+    def __init__(self):
+        self.bucket = []
+
+    def get(self, key):
+        for (k, v) in self.bucket:
+            if k == key:
+                return v
+        return -1
+
+    def update(self, key, value):
+        found = False
+        for i, kv in enumerate(self.bucket):
+            if key == kv[0]:
+                self.bucket[i] = (key, value)
+                found = True
+                break
+
+        if not found:
+            self.bucket.append((key, value))
+
+    def remove(self, key):
+        for i, kv in enumerate(self.bucket):
+            if key == kv[0]:
+                del self.bucket[i]
+                
 class MyHashMap:
 
     def __init__(self):
-        self.map = {}
+        self.key_space = 2069
+        self.hash_table = [Bucket() for i in range(self.key_space)]
         
         
 
     def put(self, key: int, value: int) -> None:
-        self.map[key] = value
+        hash_key = key % self.key_space
+        self.hash_table[hash_key].update(key, value)
+                
+                
         
 
     def get(self, key: int) -> int:
-        return self.map.get(key, -1)
+        hash_key = key % self.key_space
+        return self.hash_table[hash_key].get(key)
         
 
     def remove(self, key: int) -> None:
-        if key in self.map:
-            del self.map[key]
+        hash_key = key % self.key_space
+        self.hash_table[hash_key].remove(key)
         
 
 
