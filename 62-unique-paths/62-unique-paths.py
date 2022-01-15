8032ab760ccc56c3332080132c8dc523234da28a
Gowtham <gowthamnba09@gmail.com>
2022-01-15 11:36:08 -0800

    Time: 49 ms (14.79%), Space: 14.3 MB (66.62%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -3,12 +3,8 @@ class Solution:
         dp = [[-1 for _ in range(n)] for _ in range(m)]
         
         def ways(i, j):
-            if i == 0 and j == 0:
+            if i == 0 or j == 0:
                 return 1
-            elif i < 0:
-                return 0
-            elif j < 0:
-                return 0
             
             if dp[i][j] == -1:
                 dp[i][j] = ways(i-1, j) + ways(i, j-1)
