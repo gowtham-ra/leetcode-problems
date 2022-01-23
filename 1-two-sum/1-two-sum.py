ff55626c45a7c3605730087ae3b39495415fbff2
Gowtham <gowthamnba09@gmail.com>
2022-01-23 01:39:07 -0800

    Time: 93 ms (53.28%), Space: 15.4 MB (19.14%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,16 +2,11 @@ from collections import Counter
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary = Counter(nums)
+        hashmap = {}
         
         for i, num in enumerate(nums):
-            val = target - num
+            complement = target - num
             
-            if val in dictionary:
-                idx = nums.index(val)
-                
-                if idx != i:
-                    return [i, idx]
-                
-            
-        
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[num] = i
