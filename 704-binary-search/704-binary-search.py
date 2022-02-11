1336bc14a7c1950f1491d860311763dbb4038982
Gowtham <gowthamnba09@gmail.com>
2022-02-10 18:00:30 -0800

    Time: 282 ms (54.60%), Space: 22.8 MB (25.05%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,19 +1,38 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.binsearch(nums, target, 0, len(nums)-1)
+        def binsearch(left, right):
+            if left > right:
+                return -1
+            
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                return binsearch(mid+1, right)
+            
+            return binsearch(left, mid-1)
         
-    def binsearch(self, nums, target, start, end):
-        if start > end:
-            return -1
+        return binsearch(0, len(nums)-1)
         
-        mid = (start + end) // 2
         
-        if nums[mid] == target:
-            return mid
-        elif target > nums[mid]:
-            return self.binsearch(nums, target, mid+1, end)
-        else:
-            return self.binsearch(nums, target, start, mid-1)
+        
+        
+        
+#         return self.binsearch(nums, target, 0, len(nums)-1)
+        
+#     def binsearch(self, nums, target, start, end):
+#         if start > end:
+#             return -1
+        
+#         mid = (start + end) // 2
+        
+#         if nums[mid] == target:
+#             return mid
+#         elif target > nums[mid]:
+#             return self.binsearch(nums, target, mid+1, end)
+#         else:
+#             return self.binsearch(nums, target, start, mid-1)
     
     
         
