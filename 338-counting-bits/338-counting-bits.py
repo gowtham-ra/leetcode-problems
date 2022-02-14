302ee1c7d65ffc5e2b0e24e3e4b463ef2e7d8d16
Gowtham <gowthamnba09@gmail.com>
2022-02-13 23:17:31 -0800

    Time: 124 ms (52.73%), Space: 20.8 MB (89.76%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -3,8 +3,16 @@ class Solution:
         ans = []
         
         for num in range(n+1):
-            ans.append(bin(num)[2:].count('1'))
+            count = bin(num).count('1')
+            ans.append(count)
         
         return ans
+        
+#         ans = []
+        
+#         for num in range(n+1):
+#             ans.append(bin(num)[2:].count('1'))
+        
+#         return ans
             
         
