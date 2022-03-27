0877a7f027c17d61735d43107df92635fd2cb6f0
Gowtham <gowthamnba09@gmail.com>
2022-03-27 04:50:05 -0700

    Time: 2 ms (47.75%), Space: 41.9 MB (62.43%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,33 +1,32 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int length = Integer.MAX_VALUE;
+        if (strs == null || strs.length == 0)
+            return "";        
         
-        
-        if (strs.length == 1) return strs[0];
-        
-        for (String word : strs) {
-            length = Math.min(length, word.length());
+        int minLen = Integer.MAX_VALUE;
+        for (String str: strs) {
+            minLen = Math.min(str.length(), minLen);
         }
         
-        int count = 0;
-        boolean done = false;
-        outer:
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < strs.length-1; j++) {
-                if (strs[j].charAt(i) != strs[j+1].charAt(i)) {
-                    done = true;
-                    break;
-                }
-                
-                if (j+1 == strs.length-1) count++;
-            }
-            
-            if (done) {
-                break;
-            }
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isCommonPrefix(strs, mid))
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         
-        return strs[0].substring(0, count);
-        
+        int mid = ((low + high) / 2);
+        return strs[0].substring(0, mid);
+    }
+    
+    private boolean isCommonPrefix(String[] strs, int len) {
+        String substr = strs[0].substring(0, len);
+        for (int i = 1; i < strs.length; i++)
+            if (!strs[i].startsWith(substr))
+                return false;        
+        return true;
     }
 }
